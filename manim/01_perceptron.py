ddab7219ec027f625711a8a3a8cc333aa794eaf1
Alex Jercan <jercan_alex27@yahoo.com>
2024-06-17 20:48:58 +0300

    update

--- manim/01_perceptron.py
+++ manim/01_perceptron.py
@@ -140,3 +140,80 @@ class CostPlot(m.Scene):
         self.play(m.Create(label))
 
         self.wait(5)
+
+
+class DerivativeMinFunction(m.Scene):
+    def construct(self):
+        f = m.MathTex(r"f(w) = -5w^4 + 5w^6 + w^2 + 5")
+        min_f = m.MathTex(r"min_{f} = ?").next_to(f, m.DOWN)
+
+        self.play(m.Write(f))
+        self.play(m.Write(min_f))
+        self.wait(2)
+
+        min_f_0 = m.MathTex(r"min_{f} = w, \text{where} f'(w) = 0").next_to(f, m.DOWN)
+
+        self.play(m.TransformMatchingShapes(min_f, min_f_0, path_arc=0))
+
+        self.wait(5)
+
+
+class MinGradientDescent(m.Scene):
+    def cost_w(self, w):
+        return -5 * w ** 4 + 5 * w ** 6 + w ** 2 + 5
+
+    def construct(self):
+        ax = m.Axes(x_range=(0, 2), y_range=(4, 7), x_length=8, y_length=5)
+        y_label = ax.get_y_axis_label(
+            m.MathTex("{cost}_{w}").scale(0.65).rotate(90 * m.DEGREES),
+            edge=m.LEFT,
+            direction=m.LEFT,
+            buff=0.3,
+        )
+        x_label = ax.get_x_axis_label(
+            m.Text("w").scale(0.65),
+            edge=m.RIGHT,
+            direction=m.DOWN,
+            buff=0.3,
+        )
+
+        self.play(m.Create(ax), m.Write(x_label), m.Write(y_label))
+        self.wait(1.5)
+
+        curve = ax.plot(lambda x: self.cost_w(x), color=m.RED, x_range=(0, 1.1))
+        self.play(m.Create(curve))
+
+        self.wait(4)
+
+        random_w = 1.0
+        cost = self.cost_w(random_w)
+        x_var = m.Variable(random_w, m.Text("w"), num_decimal_places=4)
+        cost_var = m.Variable(cost, m.MathTex("{cost}_{w}"), num_decimal_places=4)
+        m.Group(x_var, cost_var).arrange(m.DOWN).next_to(curve, m.RIGHT)
+        x = x_var.tracker
+        y = cost_var.tracker
+
+        dot = m.always_redraw(lambda: m.Dot().move_to(ax.coords_to_point(x.get_value(), self.cost_w(x.get_value()))))
+        self.play(m.Write(dot), m.Write(x_var), m.Write(cost_var))
+
+        self.wait(7)
+
+        slopes = m.always_redraw(
+            lambda: ax.get_secant_slope_group(
+                x=x.get_value() - 0.0005,
+                graph=curve,
+                dx=0.001,
+                dx_line_color=m.YELLOW,
+                dy_line_color=m.GREEN,
+                secant_line_length=4,
+                secant_line_color=m.BLUE,
+            )
+        )
+
+        self.play(m.Create(slopes))
+
+        self.play(x.animate.set_value(0.5), y.animate.set_value(self.cost_w(0.5)), run_time=4)
+
+        self.play(x.animate.set_value(0.7376), y.animate.set_value(self.cost_w(0.7376)), run_time=2)
+
+        self.wait(7)
